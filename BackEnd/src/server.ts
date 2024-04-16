eb3cde03ab34c605b8d453b6950c6c8644db6581
LucasLuis-Dev <lucasluisouza@gmail.com>
2024-04-16 17:08:38 -0300

    refactor: restructuring the project for a better organization

---
+++ BackEnd/src/server.ts
@@ -0,0 +1,23 @@
+import express, { Express } from "express";
+import mainRouter from "./routes/index";
+import cors from "cors"; 
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const app: Express = express();
+
+const corsOptions ={
+  origin:'https://gpt4enem-production-1c80.up.railway.app', 
+  credentials:true,
+  optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
+  
+app.use(mainRouter);
+
+app.listen(80, () => {
+    console.log('Aplicação rodando');
+});
